3e43b55dbe96aac371a07ece25606aff8e208370
Abhishek Anand <abhishekanand7091@gmail.com>
2025-08-11 21:46:18 +0530

    Remove node_modules from repo

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app=express();
 const PORT = 8000;
 
 app.set("view engine","ejs");
-app.set("views",path.resolve);
+app.set("views",path.resolve('./views'));
 
+app.get("/",(req,res)=>{
+  res.render("home")
+})
+ 
 app.listen(PORT,()=>{
   console.log(`server start at https://localhost:${PORT}`)
 });
---
+++ models/user.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    passoword: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/default.png",
+    },
+  },
+  { timestamps: true }
+);
+
+
+const User =  model("User",userSchema);
+
+module.exports = User;
